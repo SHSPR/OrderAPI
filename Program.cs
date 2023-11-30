de947a74afe85156285956ec73ba4460411e8e30
SHSPR <maksiartur@yandex.ru>
2023-12-01 00:00:50 +0300

    Actualize

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using OrderAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+
 //builder.Services.AddDbContext<OrderDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<OrderDbContext>(opt => opt.UseInMemoryDatabase("OrderDb"));
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
